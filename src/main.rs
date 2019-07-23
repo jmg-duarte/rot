b2f55f26f3aab00669bd0bb9e02368af2225d965
Jose Duarte <jmg.duarte@campus.fct.unl.pt>
2019-07-23 17:34:07 +0100

    Implement decipher

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod caesar;
 
 const ARG_NAME_INPUT_TEXT: &'static str = "text";
 const ARG_NAME_ROTATION: &'static str = "rotation";
+const ARG_NAME_DECIPHER: &'static str = "decipher";
 
 fn main() {
     let matches = App::new("Caesar Cipher Tool")
@@ -19,6 +20,9 @@ fn main() {
                 .help("Rotation value that should be used in the process")
                 .required(true)
                 .index(2),
+            Arg::with_name(ARG_NAME_DECIPHER)
+                .help("Decipher the input text")
+                .short("d"),
         ])
         .get_matches();
 
@@ -26,7 +30,13 @@ fn main() {
     let rotation = matches.value_of(ARG_NAME_ROTATION).unwrap();
     let rot = rotation.parse::<u8>().unwrap();
 
-    println!("text:{}\n", text);
-    println!("rotation:{}\n", rot);
-    println!("result:{}\n", caesar::cipher(text, rot));
+    if matches.is_present(ARG_NAME_DECIPHER) {
+        println!("text:{}\n", text);
+        println!("rotation:{}\n", rot);
+        println!("result:{}\n", caesar::decipher(text, rot));
+    } else {
+        println!("text:{}\n", text);
+        println!("rotation:{}\n", rot);
+        println!("result:{}\n", caesar::cipher(text, rot));
+    }
 }
