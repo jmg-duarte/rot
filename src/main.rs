4d7ce05eb9e1dd7569007ff4ddba6efd07ff5329
José Duarte <jmg.duarte@campus.fct.unl.pt>
2019-07-25 14:30:45 +0100

    Extract the CLI logic to a separate file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use clap::values_t;
+
 use clap::App;
 
+mod cli;
 mod caesar;
 mod io;
 
@@ -11,54 +12,5 @@ fn main() {
         .author(clap::crate_authors!())
         .get_matches();
 
-    let text = get_text(&matches);
-    let output = process_text(&matches, &text);
-
-    if matches.is_present("output") {
-        let file = matches.value_of("output").unwrap();
-        io::write_output(file, &output).expect("Error writing file");
-    } else {
-        println!("{}", output);
-    };
-}
-
-fn get_text(matches: &clap::ArgMatches) -> String {
-    let (input, file, stdin) = (
-        matches.is_present("input"),
-        matches.is_present("file"),
-        matches.is_present("stdin"),
-    );
-
-    match (input, file, stdin) {
-        (true, _, _) => matches.value_of("input").unwrap().to_string(),
-        (_, true, _) => {
-            io::read_input(matches.value_of("file").unwrap()).expect("Error reading file")
-        }
-        (_, _, true) | (false, false, false) => io::read_stdin().unwrap(),
-    }
-}
-
-fn process_text(matches: &clap::ArgMatches, text: &str) -> String {
-    let (brutef, cipher, decipher) = (
-        matches.is_present("brutef"),
-        matches.is_present("cipher"),
-        matches.is_present("decipher"),
-    );
-
-    match (brutef, cipher, decipher) {
-        (true, _, _) => {
-            let r: Vec<u8> = (1..26).collect();
-            caesar::decipher_n(&text, &r)
-        }
-        (_, true, _) => {
-            let v = clap::values_t!(matches, "cipher", u8).unwrap();
-            caesar::cipher_n(&text, &v)
-        }
-        (_, _, true) => {
-            let v = clap::values_t!(matches, "decipher", u8).unwrap();
-            caesar::decipher_n(&text, &v)
-        }
-        (_, _, _) => unreachable!(),
-    }
-    .join("\n")
+    cli::run(matches);
 }
